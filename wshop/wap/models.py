0d6ba7982b13c4d462e630e52ef48d3778413501
Dmytro Yakovenko <dmitry.yakovenko@gmail.com>
2022-08-01 22:56:45 +0300

    first test record

--- wshop/wap/models.py
+++ wshop/wap/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
+class Stock(models.Model):
+    code = models.IntegerField(validators=[MinLengthValidator(12), MaxLengthValidator(12)], primary_key=True)
+    description = models.CharField(max_length=80)
+    plant = models.CharField(max_length=4)
+    location = models.CharField(max_length=4)
+    unrestricted = models.IntegerField()
+    valueUnrestricted = models.IntegerField()
+    blocked = models.IntegerField()
+
+    price = models.FloatField()
+    photo = models.ImageField(blank=True, null=True, default=None, upload_to='products')
+
+    def __repr__(selfself):
+        return(f'{code} {description} {unrestricted} {price}')
+
--- wshop/wap/views.py
+++ wshop/wap/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Stock
 
 # Create your views here.
+def index(request):
+    return render(request, "wap/index.html", { "stock": Stock.objects.all() })
+
+def upload(request):
+    pass
